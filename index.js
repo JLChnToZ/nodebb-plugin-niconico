1d5adfc5c9190021a1d80dfa6a4ed61c39f40471
JLChnToZ <jeremylam619@gmail.com>
2014-10-02 16:20:07 +0800

    Make the iframe responsive.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 	"use strict";
 
 	var nico = {},
-  embed = '<iframe src="/plugins/nodebb-plugin-niconico/embed/plugin.html?$1" style="border: none;"></iframe>';
+  embed = '<div class="embed-responsive embed-responsive-16by9"><iframe src="/plugins/nodebb-plugin-niconico/embed/plugin.html?$1" class="embed-responsive-item"></iframe></div>';
 
 	nico.parse = function(postContent, callback) {
 		postContent = postContent.replace(/<a href="(?:http:\/\/)?(?:www\.)?nicovideo.jp\/watch\/(.+)">.+<\/a>/g, embed);
