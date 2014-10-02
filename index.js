c91eea72146b4904a435428cd31dfe2a8f882f88
JLChnToZ <jeremylam619@gmail.com>
2014-10-02 16:13:03 +0800

    Remove the ugly iframe border.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 	"use strict";
 
 	var nico = {},
-  embed = '<iframe src="/plugins/nodebb-plugin-niconico/embed/plugin.html?$1" width="485" height="385"></iframe>';
+  embed = '<iframe src="/plugins/nodebb-plugin-niconico/embed/plugin.html?$1" style="border: none;"></iframe>';
 
 	nico.parse = function(postContent, callback) {
 		postContent = postContent.replace(/<a href="(?:http:\/\/)?(?:www\.)?nicovideo.jp\/watch\/(.+)">.+<\/a>/g, embed);
