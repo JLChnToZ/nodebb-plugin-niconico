cb8a497a299ce2997d0b45fb78d7f01c3ca5d0cc
JLChnToZ <jeremylam619@gmail.com>
2014-10-04 16:55:14 +0800

    Add support to community / mylist / user links.

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-(function(module) {
-	"use strict";
+(function(nico) {
+  "use strict";
 
-	var nico = {},
-  embed = '<div class="embed-responsive embed-responsive-16by9"><iframe src="/plugins/nodebb-plugin-niconico/embed/plugin.html?$1" class="embed-responsive-item"></iframe></div>';
+  var converts = [
+    { // Video
+      from: /<a href="(?:http:\/\/)?(?:www\.)?nicovideo.jp\/watch\/(.+)">.+<\/a>/g,
+      to: '<div class="embed-responsive embed-responsive-16by9">'+
+      '<iframe src="/plugins/nodebb-plugin-niconico/embed/plugin.html?$1" '+
+      'class="embed-responsive-item"></iframe></div>'
+    },
+    { // User / Mylist / Community
+      from: /<a href="(?:http:\/\/)?(?:www\.)?nicovideo.jp\/(user|mylist|community)\/(.+)">.+<\/a>/g,
+      to: '<iframe src="http://ext.nicovideo.jp/thumb_$1/$2" width="312" height="176"></iframe>'
+    }
+  ];
 
-	nico.parse = function(postContent, callback) {
-		postContent = postContent.replace(/<a href="(?:http:\/\/)?(?:www\.)?nicovideo.jp\/watch\/(.+)">.+<\/a>/g, embed);
-		callback(null, postContent);
-	};
+  nico.parse = function(postContent, callback) {
+    for(var i = 0; i < converts.length; i++)
+      postContent = postContent.replace(converts[i].from, converts[i].to);
+    callback(null, postContent);
+  };
 
-	module.exports = nico;
-}(module));
+})(module.exports);
